3a16df9907290a0013fd84eb3ad2d64280e470d3
Kjell Winblad <kjellwinblad@gmail.com>
2016-06-03 23:07:52 +0200

    New adoption values that seems to work well

--- lib/qdcatree/skiplist_fpaqdcatree_set.c
+++ lib/qdcatree/skiplist_fpaqdcatree_set.c
@@ -800,14 +800,14 @@ static inline void delegate_perform_remove_min_with_lock(unsigned int msgSize, v
  *=================
  */
 #define MAX_PUT_BUFFER_SIZE 1024
-#define PUT_BUFF_INCREASE_VALUE 10
+#define PUT_BUFF_INCREASE_VALUE 500
 #define PUT_BUFF_DECREASE_VALUE 1
 static inline void adjust_put_buffer(){
     //Both buffers are empty reset put buffer and check if tresholds for put buffer limits are reached
-    if(fpahelp_info.put_contention > 100){
+    if(fpahelp_info.put_contention > 10){
         fpahelp_info.put_contention = 0;
-        if(fpahelp_info.max_buffered_puts < (MAX_PUT_BUFFER_SIZE-10)){
-            fpahelp_info.max_buffered_puts = fpahelp_info.max_buffered_puts + 10;
+        if(fpahelp_info.max_buffered_puts < (MAX_PUT_BUFFER_SIZE-PUT_BUFF_INCREASE_VALUE)){
+            fpahelp_info.max_buffered_puts = fpahelp_info.max_buffered_puts + PUT_BUFF_INCREASE_VALUE;
             //printf("%d ", fpahelp_info.max_buffered_puts);
         }
     }else if (fpahelp_info.put_contention < -10){
