d686bdacd10cf987082122125d0f4e265c8ae3c6
Kjell Winblad <kjellwinblad@gmail.com>
2016-06-29 17:16:22 +0200

    Updated run seq script

--- run_seq.sh
+++ run_seq.sh
@@ -1,33 +1,64 @@
 #!/bin/sh
 
-echo roadnet unweighted
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -o seq fibheap
+# echo roadnet unweighted
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -o seq fibheap
 
-echo roadnet weight 1000
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000 -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000 -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000 -o seq fibheap
+# echo roadnet weight 1000
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000 -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000 -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000 -o seq fibheap
 
-echo roadnet weight 1000000
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000000 -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000000 -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000000 -o seq fibheap
+# echo roadnet weight 1000000
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000000 -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000000 -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i roadNet-CA.txt -w 1000000 -o seq fibheap
 
 
-echo live unweighted
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -o seq fibheap
+# echo live unweighted
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -o seq fibheap
 
-echo live weight 1000
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000 -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000 -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000 -o seq fibheap
+# echo live weight 1000
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000 -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000 -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000 -o seq fibheap
 
-echo live weight 1000000
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000000 -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000000 -o seq fibheap
-build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000000 -o seq fibheap
+# echo live weight 1000000
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000000 -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000000 -o seq fibheap
+# build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i soc-LiveJournal1.txt -w 1000000 -o seq fibheap
 
+
+echo rand n 10000 p 0.5 unweighted
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -o seq fibheap
+
+echo rand n 10000 p 0.5 weigh 1000
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000 -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000 -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000 -o seq fibheap
+
+echo rand n 10000 p 0.5 weight 1000000
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000000 -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000000 -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000000 -o seq fibheap
+
+
+echo rand n 1000000 p 0.0001 unweighted
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -o seq fibheap
+
+echo rand n 1000000 p 0.0001 weigh 1000
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000 -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000 -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000 -o seq fibheap
+
+echo rand n 1000000 p 0.0001 weight 1000000
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000000 -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000000 -o seq fibheap
+build/src/bench/file_shortest_paths_seq_no_lazy_relax -n 1 -i rand_graph3.txt -w 1000000 -o seq fibheap
