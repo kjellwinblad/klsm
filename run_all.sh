d1998fe88435dba2ebf9ca99f831fb8c71965ac3
Kjell Winblad <kjellwinblad@gmail.com>
2016-07-06 11:00:25 +0200

    Updated which data structures that is included in the default benchmark.

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#linden spraylist qdcatree fpaqdcatree fpaqdcatreenoputadapt fpaqdcatreenormminadapt fpaqdcatreenocatreeadapt klsm2048 klsm4096 klsm8192 klsm16384 multiqC2 multiqC4 multiqC8 multiqC16
-for DATASTRUCTURE in linden spraylist qdcatree fpaqdcatree fpaqdcatreenoputadapt fpaqdcatreenormminadapt fpaqdcatreenocatreeadapt klsm2048 klsm65536 globallock dlsm multiqC8 multiqC16
+
+for DATASTRUCTURE in fpaqdcatree fpaqdcatreenoputadapt fpaqdcatreenormminadapt qdcatree fpaqdcatreenocatreeadapt linden spraylist klsm1024 klsm65536 globallock multiqC16
 do
-	python3.4 bench_file_shortest_path.py 1 $DATASTRUCTURE
+	python bench_file_shortest_path.py 1 $DATASTRUCTURE
 done
