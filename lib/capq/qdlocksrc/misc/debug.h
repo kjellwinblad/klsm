0bdb9577b54d240c089bc9a246e69d8a23917204
Kjell Winblad <kjellwinblad@gmail.com>
2017-03-12 14:10:32 +0000

    Add LICENSE information to qd_lock_lib

--- lib/capq/qdlocksrc/misc/debug.h
+++
@@ -1,4 +0,0 @@
-            for(int i = index; i < messageEndOffset; i++){
-                printf("%02x", ((unsigned char)(q->buffer[i])) );
-            //printf("%2X", ((unsigned char *) q->buffer)[i] );
-            }
--- lib/capq/qdlocksrc/read_indicators/reader_groups_read_indicator.h
+++ lib/capq/qdlocksrc/read_indicators/reader_groups_read_indicator.h
@@ -1,3 +1,22 @@
+/*
+ *  Copyright 2017 Kjell Winblad
+ *
+ *  This file is part of qd_lock_lib.
+ *
+ *  qd_lock_lib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  qd_lock_lib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with qd_lock_lib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef READER_GROUPS_READ_INDICATOR_H
 #define READER_GROUPS_READ_INDICATOR_H
 
